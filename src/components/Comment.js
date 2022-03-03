5d240927769e3f5e5d0da6b043158685e3f06acb
leandrob0 <leandro.bovino@gmail.com>
2022-03-03 11:22:46 -0300

    comment styling for the user that sees its own comment

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,17 +1,17 @@
 import { useSelector } from "react-redux";
 
 const Comment = ({ author, text }) => {
-  const isAdmin = useSelector((state) => state.user.value.admin);
+  const user = useSelector((state) => state.user.value);
 
   return (
     <div className="py-2 border-b">
-      {isAdmin ? (
+      {user.username === author ? (
         <h3 className="text-blue-800">
-          <em>{author}</em>
+          <b>{author}</b>
         </h3>
       ) : (
         <h3 className="text-blue-600">
-          <em>{author}</em>
+          {author}
         </h3>
       )}
       <p>{text}</p>
